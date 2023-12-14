95f51b19aa6097c7d7f6f9797031bf7f3ab78985
dandan <itsdansantos.qpax@gmail.com>
2023-12-14 20:17:15 +0800

    HomeView

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div class="wrapper">
+      <div>
+      </div>
+      <div class="nav-links">
+          <RouterLink to="/" class="router">Home</RouterLink>
+          <RouterLink to="/about" class="router">About</RouterLink>
+      </div>
+    </div>
+  <RouterView />
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&family=Rubik:wght@300;400;500;700&display=swap');
+*{
+  font-family: 'Roboto', sans-serif;
+  color: white;
+}
+.wrapper{
+  width: 100%;
+  display: grid;
+  grid-template-columns: 88% 12%;
+  padding: 20px 10px;
+}
+.router {
+  font-family: 'Roboto', sans-serif;
+  text-decoration: none;
+  color: inherit;
+}
+.nav-links {
+  display: flex;
+  justify-content:space-around;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,66 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1 class="heading">Hello User!</h1>
+    <p class="heading-sub">A website made by Dan</p>
+    <br>
+    <br>
+    <div class="games">
+      <RouterLink to="/TodoList" class="router">
+      <div class="game-container game1"></div>
+      </RouterLink>
+      <div class="game-container">
+  
+      </div>
+      <div class="game-container">
+  
+      </div>
+      <div class="game-container">
+  
+      </div>
+      <div class="game-container">
+  
+      </div>
+      <div class="game-container">
+  
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&family=Roboto:wght@100;300;400;700&family=Rubik:wght@300;400;500;700&display=swap');
+.heading{
+  text-align: center;
+  font-family: 'Kalam', cursive;
+  font-size: 70px;
+}
+.heading-sub{
+  margin-top: -4%;
+  text-align: center;
+  font-size: 15px;
+}
+.games{
+  padding: 0 50px ;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+}
+.game-container{
+  position: relative;
+  height: 250px;
+  background-color: white;
+  border: 4px solid black;
+  border-radius: 15px;
+}
+
+.game-container:hover {
+  transform: scale(1.02); /* Increase size by 2% on hover */
+}
+.game1{
+  background: url('../assets/todo.jpg') center/contain no-repeat;
+  background-color: white;
+}
+</style>
